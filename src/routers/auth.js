2dbaf2e0a2e639af05c5ea36118b8232084ca141
Prosper <anosikeprosper2@gmail.com>
2023-07-01 08:51:54 +0100

    added passport middleware to reset password

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,15 +3,20 @@ const express = require('express');
 const { signupDto } = require('../validators/user');
 const { signUp, resetPassword,  } = require('../controllers/auth');
 const { resetDto } = require('../validators/resetPassword');
+const{verifyUser} = require('../middlewares/auth')
 const authRouter = express.Router();
 
 authRouter
  .route('/signup')
  .post(signupDto, signUp);
 
+//  authRouter
+//  .route('/login')
+//  .post(verifyUser, login);
+
  authRouter
  .route('/reset/password')
- .post(resetDto, resetPassword);
+ .post(verifyUser,resetDto, resetPassword);
 
 //  authRouter
 //  .route('/reset/password')
